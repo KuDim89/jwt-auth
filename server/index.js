48054192ff34c71907f8aba2bc06281bf5cedfa7
dkukharuk <dmytro.kukharuk@avenga.com>
2023-04-07 13:08:29 +0300

    set port in env variables

--- server/index.js
+++ server/index.js
@@ -1,13 +1,14 @@
+require('dotenv').config();
 const express = require('express');
 //const cors = require('cors');
 //const cookieParser = require('cookie-parser');
 
-const PORT = 5000;
+const port = process.env.PORT || 5000;
 const app = express();
 
 const start = async () => {
   try {
-    app.listen(PORT, () => console.log(`Server started on PORT = ${PORT}`))
+    app.listen(port, () => console.log(`Server started on PORT = ${port}`))
   } catch (e) {
     console.log(e)
   }
